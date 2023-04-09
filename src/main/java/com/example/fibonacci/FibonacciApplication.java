f86c5896319a82285956118dd145e9618d25c4b9
kevin <kevinrjauretche@gmail.com>
2023-04-09 16:20:26 -0300

    Add PORT env variable

--- src/main/java/com/example/fibonacci/FibonacciApplication.java
+++ src/main/java/com/example/fibonacci/FibonacciApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FibonacciApplication {
 
+	public String PORT = System.getenv("PORT");	
 	public static void main(String[] args) {
 		SpringApplication.run(FibonacciApplication.class, args);
 	}
